49ab2ddcdc5f6570537add83c0b177010fec7180
mohasim328 <mohasim3283@gmail.com>
2024-10-15 07:56:51 +0530

    mailtarp

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -10,7 +10,7 @@ export const mailtrapClient = new MailtrapClient({
 });
 
 export const sender = {
-  email: "mohasim3283@gmail.com",
+  email: "mailtarp@demomailtrap.com",
   name: "noreply",
 };
 
